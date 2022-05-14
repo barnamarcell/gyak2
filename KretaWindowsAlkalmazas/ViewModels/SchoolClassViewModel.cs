9308c14e55c8618f4dea97ba55330e77a9c1a984
barnamarcell <78147578+barnamarcell@users.noreply.github.com>
2022-05-14 17:22:02 +0200

    Initial commit

---
+++ KretaWindowsAlkalmazas/ViewModels/SchoolClassViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kreta.Models;
+using Kreta.Repositories;
+using System.Collections.ObjectModel;
+
+namespace Kreta.ViewModels
+{
+    public class SchoolClassViewModel
+    {
+        private SchoolClassesRepo classesRepo;
+        private ObservableCollection<SchoolClass> classes;
+
+        public SchoolClassViewModel()
+        {
+            classesRepo = new SchoolClassesRepo();
+            classes = new ObservableCollection<SchoolClass>(classesRepo.SchoolClasses);
+        }
+
+        public ObservableCollection<SchoolClass> Classes { get => classes; set => classes = value; }
+    }
+}
